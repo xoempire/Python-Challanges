cfe7f5501664a8ba4526f0380eca40ae7adf69f3
xoempire <68499770+xoempire@users.noreply.github.com>
2021-03-15 11:59:45 +0330

    The board for XO

---
+++ 24_Tic_tac_toe_board.py
@@ -0,0 +1,16 @@
+# logic:
+# printing 4 sets of vertical lines and 4 sets of horizontal lines
+h=" ----  ----  ----"
+v='|    |     |     |'
+b=0
+while b <3:
+    print(h)
+    print(v)
+    b+=1
+print(h)
+
+
+
+
+
+
